20e0649a059b2e906609a29360f072825381e752
azuredevops7 <azuredevopsshammu@gmail.com>
2026-01-08 15:43:36 +0530

    Update ecr.sh

--- ecr.sh
+++ ecr.sh
@@ -1,7 +1,7 @@
-aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 978163710174.dkr.ecr.ap-southeast-2.amazonaws.com
+aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 978163710174.dkr.ecr.us-west-1.amazonaws.com
 
-docker build -t testshammuecr .
+docker build -t shammuuu .
 
-docker tag testshammuecr:latest 978163710174.dkr.ecr.ap-southeast-2.amazonaws.com/testshammuecr:latest
+docker tag shammuuu:latest 978163710174.dkr.ecr.us-west-1.amazonaws.com/shammuuu:latest
 
-docker push 978163710174.dkr.ecr.ap-southeast-2.amazonaws.com/testshammuecr:latest
+docker push 978163710174.dkr.ecr.us-west-1.amazonaws.com/shammuuu:latest
