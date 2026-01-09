c669a6aac6d6925fb6b49ae17c206b2aa8e5af79
azuredevops7 <azuredevopsshammu@gmail.com>
2026-01-09 18:05:26 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,4 +1,4 @@
-mvn sonar:sonar \
-  -Dsonar.projectKey=maven \
-  -Dsonar.host.url=http://13.239.1.158:9000 \
-  -Dsonar.login=3580df05ddd9a280b47d01922f8abdb707a46e05
+    mvn sonar:sonar \
+       -Dsonar.projectKey=maven \
+       -Dsonar.host.url=http://13.57.13.251:9000 \
+       -Dsonar.login=1900ee7bf8ee33084b01d28cb070d88d5f32f169
